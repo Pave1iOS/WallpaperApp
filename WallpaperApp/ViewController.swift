f0a404ba700daf42b0b35aa27f3e3ca16822f288
Pavel Gribachev <PGribachev@yandex.ru>
2024-01-31 16:04:05 +0300

    Initial Commit

---
+++ WallpaperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallpaperApp
+//
+//  Created by Pavel Gribachev on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
